7917bbea9d29d84d235f15a23dff909445ade362
Sally Haefling <sallyhaefling@gmail.com>
2019-07-27 13:35:32 -0600

    Add search bar and title

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id = 'app'>
+  <div class = 'header'>
+  <div class = 'search'>
+    <h1 class = 'title'>Searchy Syns</h1>
+    <Search v-on:display-syn="displaySyn" />
+  </div>
+  </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Search from './components/Search'
+import apiKey from '../apiKey'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Search
+  },
+  data () {
+    return {
+      key: apiKey,
+      syns: []
+    }
+  },
+  methods: {
+    async displaySyn (syn) {
+      const url=`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/word?key=${apiKey}`
+      try {
+        const data = await fetch(url)
+        const syns = await data.json()
+        this.syns = syns.results
+      } catch (error) {
+        console.log(error)
+      }
+    }
   }
 }
 </script>
@@ -19,10 +42,20 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+
+.header {
+  display: flex;
+}
+
+.title {
+  font-size: 40px;
+  display: flex;
+  color: hsl(241, 81%, 64%);
+}
+
+.search {
+  padding-left: 30px;
 }
 </style>
